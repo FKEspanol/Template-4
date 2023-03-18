191bb2ccbc52a9639136e30fbbe9aa84a6c3940e
Francis Karlo Espanol <karlsgit1126@gmail.com>
2023-03-18 17:19:12 +0800

    commit changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-image": "url('/public/images/hero-image.png')",
+        "hero-image": "url('/public/images/hero-image.jpg')",
       },
     },
   },
